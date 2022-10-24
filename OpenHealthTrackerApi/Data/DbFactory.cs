acd6bc8a6a449c6e14c2137e3c6c0a2a3aab36f8
Gradyn Wursten <me@gradyn.com>
2022-10-23 19:45:32 -0600

    Initial commit

---
+++ OpenHealthTrackerApi/Data/DbFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace OpenHealthTrackerApi.Data;
+
+public class DbFactory
+{
+    private IConfiguration _config;
+
+    public DbFactory(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public OHTDbContext OHT()
+    {
+        return new OHTDbContext(_config);
+    }
+}
---
+++ OpenHealthTrackerApi/Data/OHTDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OpenHealthTrackerApi.Data;
+
+public class OHTDbContext : DbContext
+{
+    private IConfiguration _config;
+
+    public OHTDbContext(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql($"Host={_config.GetValue<string>("Database:Host")};Database={_config.GetValue<string>("Database:Database")};Username={_config.GetValue<string>("Database:Username")};Password={_config.GetValue<string>("Database:Password")}");
+}
