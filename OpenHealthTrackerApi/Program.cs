33626bf8a9401be71ae226ec11ba1bfccb59879d
Gradyn Wursten <me@gradyn.com>
2023-01-21 11:31:47 -0700

    authentication ground work

--- OpenHealthTrackerApi/Program.cs
+++ OpenHealthTrackerApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOpenIdConnect(o =>
+    {
+        o.ClientId = builder.Configuration.GetValue<string>("IdentityStore:ClientId");
+        o.ClientSecret = builder.Configuration.GetValue<string>("IdentityStore:ClientSecret");
+        o.Authority = builder.Configuration.GetValue<string>("IdentityStore:Authority");
+        o.GetClaimsFromUserInfoEndpoint = true;
+        o.RequireHttpsMetadata = true;
+    });
 
 var app = builder.Build();
 
@@ -21,5 +38,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// OIDC
+
 
 app.Run();
