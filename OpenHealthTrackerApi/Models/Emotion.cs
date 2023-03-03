db1f174e49a24fdd5e30f8e74a594bc672215f8f
Gradyn Wursten <me@gradyn.com>
2023-03-03 13:05:01 -0700

    remove obselete psudo-property from model

--- OpenHealthTrackerApi/Models/Emotion.cs
+++ OpenHealthTrackerApi/Models/Emotion.cs
@@ -6,10 +6,6 @@ public class Emotion
     public string Name { get; set; }
     public EmotionCategory Category { get; set; }
     public string Icon { get; set; }
-    public bool IsCustom
-    {
-        get { return Icon.StartsWith("/"); }
-    }
-    
+
     public string IconType { get; set; }
 }
